03e6f2cdfc1a7f58ff2f0c1247c6454f5ee98090
dracangelo <kelvinmbuguaw@gmail.com>
2020-02-04 11:33:25 +0300

    redeploying

--- pictures/settings.py
+++ pictures/settings.py
@@ -128,6 +128,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
